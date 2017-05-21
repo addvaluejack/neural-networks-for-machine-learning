27cf85d09243123aa25a0f4bbfa60407e95aeb78
Muye Jiang <addvaluejack@gmail.com>
2017-05-21 09:40:25 -0400

    Completed ex4

--- ex4/configuration_goodness.m
+++ ex4/configuration_goodness.m
@@ -3,5 +3,10 @@ function G = configuration_goodness(rbm_w, visible_state, hidden_state)
 % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+    tmpG = 0;
+    for index = 1:size(visible_state, 2)
+        tmp = hidden_state(:, index) * (visible_state(:, index))';
+        tmpG = tmpG + sum(sum(rbm_w .* tmp));
+    end
+    G = tmpG / size(visible_state, 2);
 end
